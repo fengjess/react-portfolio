f08c4d27edfe64c8710b80e05b45e77914762a86
Jessica Feng <jessicafeng14@yahoo.com>
2020-11-07 21:50:42 -0800

    Updates

---
+++ precache-manifest.4a2e33c22c48c5fda01ca7da988a5638.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d45c1c2ab88cc0ed3d96971848fff10",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "5f2ea14a8ed31ea7d883",
+    "url": "/react-portfolio/static/css/main.585eb34a.chunk.css"
+  },
+  {
+    "revision": "86262f5312fd9c96d804",
+    "url": "/react-portfolio/static/js/2.fb81c96a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-portfolio/static/js/2.fb81c96a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2ea14a8ed31ea7d883",
+    "url": "/react-portfolio/static/js/main.06698ee3.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "474edc703cca6605d3d90d31d35a233f",
+    "url": "/react-portfolio/static/media/bg.474edc70.png"
+  },
+  {
+    "revision": "7907a35a8e0fe378491962eb1807c09c",
+    "url": "/react-portfolio/static/media/code-quiz.7907a35a.png"
+  },
+  {
+    "revision": "6d1540f852a4289f17a475e71b74edda",
+    "url": "/react-portfolio/static/media/day-planner.6d1540f8.png"
+  },
+  {
+    "revision": "755f31b4f27343f34c2d5c9db0fbc0e3",
+    "url": "/react-portfolio/static/media/eat-da-burger.755f31b4.png"
+  },
+  {
+    "revision": "d5bb4ca120191f572c7bb74af8253687",
+    "url": "/react-portfolio/static/media/jessfeng.d5bb4ca1.JPG"
+  },
+  {
+    "revision": "ca6014e9109da2455cd9d4f7900b013e",
+    "url": "/react-portfolio/static/media/nomstop.ca6014e9.png"
+  },
+  {
+    "revision": "d03fc4b128af40673c08e7705cabab04",
+    "url": "/react-portfolio/static/media/note-taker.d03fc4b1.png"
+  },
+  {
+    "revision": "1d6c4fee50042df7f23899ea0e624d9f",
+    "url": "/react-portfolio/static/media/weather-app.1d6c4fee.png"
+  }
+]);
